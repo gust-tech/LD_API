65afa36b8be918a1e96c81ef3276445e1f9b6d60
Lady Debug <117478012+Equipe03-Generation@users.noreply.github.com>
2022-12-02 21:06:50 -0300

    Update usuario.entity.ts

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, MinLength, MaxLength } from "class-validator";
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Postagem } from "../../postagem/entities/postagem.entity";
 import { Tema } from "../../tema/entities/tema.entity";
@@ -9,31 +9,28 @@ import { Tema } from "../../tema/entities/tema.entity";
 
         @PrimaryGeneratedColumn()
         @ApiProperty()
-        id: number;
-
+        id: number
+        
         @IsNotEmpty()
         @Column({length: 255, nullable: false})
         @ApiProperty()
-        nome: string;
+        nome: string
 
         @IsEmail()
         @IsNotEmpty()
-        @MaxLength(255)
-        @Column({ length: 255, nullable: false })
-        @ApiProperty({ example: "email@email.com" })
-        usuario: string; //E-mail
+        @Column({length: 255, nullable: false})
+        @ApiProperty()
+        usuario: string // email
 
         @IsNotEmpty()
         @MinLength(8)
-        @Column({ length: 255, nullable: false })
+        @Column({length: 255, nullable: false})
         @ApiProperty()
-        senha: string;
-    
-        @IsNotEmpty()
-        @MaxLength(2000)
-        @Column({ length: 2000, nullable: false })
+        senha: string
+
+        @Column({length: 5000})
         @ApiProperty()
-        foto: string;
+        foto: string
 
         @ApiProperty({ type: () => Postagem})
         @OneToMany( () => Postagem, (postagem) => postagem.usuario)
