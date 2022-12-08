c4b546d027ec56cfbbadc7e0f4f62c5d79ded6e4
Lady Debug <117478012+Equipe03-Generation@users.noreply.github.com>
2022-12-08 14:38:14 -0300

    Add files via upload

--- src/tema/controller/tema.controller.ts
+++ src/tema/controller/tema.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards, Delete } from "@nestjs/common";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 import { Tema } from "../entities/tema.entity";
@@ -49,3 +49,4 @@ export class TemaCrontoller{
     }
 
 }
+
--- src/tema/service/tema.service.ts
+++ src/tema/service/tema.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { ILike, Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Tema } from "../entities/tema.entity";
 
 @Injectable()
