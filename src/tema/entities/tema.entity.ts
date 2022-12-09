7cc542ee1d7632065a1f7e1afcbbbb718fd4ebf0
Lady Debug <117478012+Equipe03-Generation@users.noreply.github.com>
2022-12-09 19:02:03 -0300

    Update tema.entity.ts

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -13,12 +13,12 @@ import { Postagem } from "../../postagem/entities/postagem.entity";
         @IsNotEmpty()
         @Column({length: 100, nullable: false})
         @ApiProperty()
-        assunto: string
+        descricao: string
 
         @IsNotEmpty()
         @Column({length: 100, nullable: false})
         @ApiProperty()
-        descricao: string
+        assunto: string
         
         @OneToMany(() => Postagem, (postagem) => postagem.tema)
         postagem: Postagem[]
