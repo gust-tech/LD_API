478b6edd8416882e4263b654fe3876f25ae56eb6
Lady Debug <117478012+Equipe03-Generation@users.noreply.github.com>
2022-12-09 18:58:07 -0300

    Update tema.entity.ts

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -13,7 +13,7 @@ import { Postagem } from "../../postagem/entities/postagem.entity";
         @IsNotEmpty()
         @Column({length: 100, nullable: false})
         @ApiProperty()
-        titulo: string
+        assunto: string
 
         @IsNotEmpty()
         @Column({length: 100, nullable: false})
