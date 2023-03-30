2754749457ad1cccae84cd1729f7967ab9a3d032
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2023-03-30 12:11:26 -0300

    Pequenas Correções

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import { Comentario } from './comentario/entities/comentario.entity';
+import { ComentarioModule } from './comentario/comentario.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -6,35 +8,38 @@ import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { Tema } from './tema/entities/tema.entity';
 
 @Module({
       imports: [
-  // TypeOrmModule.forRoot({
-  //   type: 'mysql',
-  //   host: 'localhost',
-  //   port: 3306,
-  //   username: 'root',
-  //   password: 'root',
-  //   database: 'db_forum',
-  //   entities: [Postagem, Tema, Usuario],
-  //   synchronize: true
-  //}),
-
   TypeOrmModule.forRoot({
-    type: 'postgres',
-    url: process.env.DATABASE_URL,
-    logging: false,
-    dropSchema: false,
-    ssl: {
-      rejectUnauthorized: false
-    },
-    synchronize: true,
-    autoLoadEntities: true,
-   }),
-    PostagemModule,
-    TemaModule,
-    UsuarioModule,
-    AuthModule
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'db_ladydebug',
+    entities: [Postagem, Tema, Usuario, Comentario],
+    synchronize: true
+  }),
+
+  // TypeOrmModule.forRoot({
+  //   type: 'postgres',
+  //   url: process.env.DATABASE_URL,
+  //   logging: false,
+  //   dropSchema: false,
+  //   ssl: {
+  //     rejectUnauthorized: false
+  //   },
+  //   synchronize: true,
+  //   autoLoadEntities: true,
+  //  }),
+  //   PostagemModule,
+  //   TemaModule,
+  //   UsuarioModule,
+  //   ComentarioModule,
+  //   AuthModule
   ],
 
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Fórum Dev Girls!';
+    return 'Blog Lady Debug!';
   }
 }
